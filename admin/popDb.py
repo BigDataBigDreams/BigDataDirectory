97274be82717fe5a2e4f3b3de86c1d09bf31788a
jableader <jacobdunk@gmail.com>
2014-07-12 18:58:03 +1000

    still trying mining

--- admin/popDb.py
+++ admin/popDb.py
@@ -1,4 +1,11 @@
 import csv
+import MySQLdb
+
+db = MySQLdb.connect('localhost','govhack', 'gov', 'govhack')
+cur = db.cursor()
+
+def ex(sql):
+	cur.execute(sql)
 
 class DataSet:
 	def __init__(self, name, pref, keys):
@@ -7,14 +14,14 @@ class DataSet:
 		self.pref = pref
 		
 	def createString(self):
-		s = "create table %(name)s ("
-		s += "%(pref)s_id int not null auto increment,"
-		s += "%(pref)s_brand varchar(32),"
-		s += "%(pref)s_model varchar(32),"
-		s += "%(pref)s_power decimal(4,2),"
-		s += "%(pref)s_star tinyint,"
-		s += "PRIMARY KEY (%(pref)s_id));"
-		return s %({'name':self.name,'pref':self.pref})
+		s = """create table %(name)s (
+		%(pref)s_id mediumint not null auto_increment,
+		%(pref)s_brand varchar(32),
+		%(pref)s_model varchar(32),
+		%(pref)s_power decimal(4,2),
+		%(pref)s_star decimal(2,1),
+		PRIMARY KEY (%(pref)s_id));"""
+		return s.replace('\\w+', ' ') %({'name':self.name,'pref':self.pref})
 		
 	def colArray(self):
 		cols = {}
@@ -29,7 +36,42 @@ class DataSet:
 				for dbKey in self.keys:
 					cols[dbKey].append(row[indexs[dbKey]])
 		return cols
+		
+	def createTable(self):
+		ex(self.createString())
+	
+	def formatVal(key, val):
+		if key in ["brand","model"]:
+			return '"'+val+'"'
+		return val
+	
+	def insert(self):
+		s = ""
+		cols = self.colArray()
+		length = len(cols['model'])
+		ind = 0
+		for j in xrange(0, length):
+			order = []
+			for key in self.keys:
+				if cols[key][order] != "":
+					order.append(key)
+			sql = "insert into (" + ",".join(order)") values ("
+			sql += ",".join([formatRow(key, cols[key][j]) for k in order])
+			sql += ");"
+			ex(sql)
+			ind += 1
+			print ind
+			
 
 keys = {'brand':'Brand','model':'Model_No','star':'Star2010_Heat','power':'H-Power_Inp_Rated'}
-dset = DataSet('airconditioner', 'ac', keys)
-print(dset.colArray())
+dset = DataSet('airconditioner', 'ac', keys)	
+
+try:
+	dset.createTable()
+	dset.insert()
+except Exception as e:
+	print "uh oh "
+	print e
+
+#with open('c:\\wamp\\www\\data\\dump.txt', 'w') as fout:
+#	fout.write(dset.createString())
