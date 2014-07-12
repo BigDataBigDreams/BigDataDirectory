401331b205118ab2066c4e999196288f009493ae
jableader <jacobdunk@gmail.com>
2014-07-12 13:30:48 +1000

    collecting data

--- data/tojson.py
+++ data/tojson.py
@@ -1,12 +1,16 @@
 import csv
 import json
 
-with open("mycsv.csv", "r") as csvFile, open("myjs.js", "w") as jsFile:
+with open("c:\\wamp\\www\\data\\2012.csv", "r") as csvFile, open("c:\\wamp\\www\\data\\2012.js", "w") as jsFile:
 	reader = csv.reader(csvFile)
 	prefix = "["
 	for row in reader:
-		rowsToKeep = [row[j] for j in [0, 1, 2, 17, 28, 29, 39, 65]]
-		jsFile.write(prefix + json.dumps(rowsToKeep))
+		colsToKeep = [row[0], row[1]]
+		for j in [2, 17, 28, 29, 39, 65]:
+			try:
+				colsToKeep.append(int(row[j].replace(',', '')))
+			except:
+				colsToKeep.append(0)
+		jsFile.write(prefix + json.dumps(colsToKeep))
 		prefix = ",\n"
 	jsFile.write("]")
-	
